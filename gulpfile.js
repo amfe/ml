994b12e502650546df4f821baa35a7372a286b11
terrykingcha <terrykingcha@gmail.com>
2014-09-10 16:54:56 +0800

    更新子模块

--- gulpfile.js
+++ gulpfile.js
@@ -10,18 +10,21 @@ var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var cssmin = require('gulp-cssmin');
 
-
-var jsfiles = ['rem/rem.js'];
-var cssfiles = ['rem/rem.css'];
+var jsfiles = ['lib.flexible/flexible.js'];
+var cssfiles = ['lib.flexible/flexible.css'];
 
 fs.readdirSync(BUILD_PATH).filter(function(dir) {
     return !!dir.match(/^lib|ctrl/);
 }).forEach(function(dir) {
+    var filepath;
     var name = dir.match(/^(?:lib|ctrl)\.(.+)$/)[1];
-    var filepath = path.join(dir, name + '.js');
+    if (name === 'flexible') return;
+
+    filepath = path.join(dir, name + '.js');
     if (fs.existsSync(filepath)) {
         jsfiles.push(filepath);
     }
+
     filepath = path.join(dir, name + '.css');
     if (fs.existsSync(filepath)) {
         cssfiles.push(filepath);
@@ -44,12 +47,10 @@ gulp.task('build', function() {
 gulp.task('dev-build', function() {
     gulp.src(jsfiles)
         .pipe(concat('ml.js'))
-        //.pipe(uglify())
         .pipe(gulp.dest(BUILD_PATH));
 
     gulp.src(cssfiles)
         .pipe(concat('ml.css'))
-        //.pipe(cssmin())
         .pipe(gulp.dest(BUILD_PATH));
 });
 
